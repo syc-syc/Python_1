155dd4a11f4cbeb1913d2fe59edc3b62841a45e2
syc-syc <81947888+syc-syc@users.noreply.github.com>
2021-04-14 19:32:40 +0900

    Add files via upload

---
+++ B777038수위천산술 연산자.py
@@ -0,0 +1,16 @@
+money,c500,c100,c50,c10=0,0,0,0,0
+money=int(input("교환할 돈은 얼마?"))
+c500=money//500
+money%=500
+c100=money//100
+money%=100
+c50=money//50
+money%=50
+c10=money//10
+money%=10
+
+print("\n500원짜리==> %d개" % c500)
+print("\n100원짜리==> %d개" % c100)
+print("\n50원짜리==> %d개" % c50)
+print("\n10원짜리==> %d개" % c10)
+print("\n바꾸지 못한 잔돈==> %d개" %money)
